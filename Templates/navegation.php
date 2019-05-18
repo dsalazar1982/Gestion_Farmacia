1639d461da95fe1aaa20a77e40db4f5a77814118
Cristian Moreno <morenoc225@gmail.com>
2019-05-18 00:19:41 -0500

    Actuaizacion de fichero navegation.php

--- Templates/navegation.php
+++ Templates/navegation.php
@@ -25,10 +25,10 @@
         <li><a href="#"><i class="fa fa-male"></i> <span>Empleados</span></a></li>
         <li><a href="#"><i class="fa fa-calculator"></i> <span>Facturas</span></a></li>
 	    <li><a href="#"><i class="fa  fa-money"></i> <span>Ventas</span></a></li>
-		<li><a href="#"><i class="fa fa-hospital-o"></i> <span>Farmacias</span></a></li>
+		<li><a href="./Vista/Farmacias/farmacia.php"><i class="fa fa-hospital-o"></i> <span>Farmacias</span></a></li>
 		<li><a href="#"><i class="fa fa-table"></i> <span>Inventario</span></a></li>
 	   <li><a href="#"><i class="fa fa-hourglass-2"></i> <span>Ofertas</span></a></li>
-		<li><a href="#"><i class="fa fa-usd"></i> <span>Nomina</span></a></li>
+		<li><a href="./Vista/Nomina/nomina.php"><i class="fa fa-usd"></i> <span>Nomina</span></a></li>
 		<li><a href="./Vista/Paises/pais.php"><i class="fa fa-globe"></i> <span>Paises</span></a></li>
 		<li><a href="#"><i class="fa  fa-plus-square"></i> <span>Productos</span></a></li>
 		<li><a href="./Vista/Propietario/propietario.php"><i class="fa fa-black-tie"></i> <span>Propietarios</span></a></li>
